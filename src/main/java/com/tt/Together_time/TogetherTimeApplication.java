f8d902cf508cc45fe28c5cb351804604f5ec5b52
karam Choi <riverchoi2523@gmail.com>
2024-11-05 16:25:44 +0900

    [ADD]프로젝트 생성

---
+++ src/main/java/com/tt/Together_time/TogetherTimeApplication.java
@@ -0,0 +1,13 @@
+package com.tt.Together_time;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TogetherTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TogetherTimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tt/Together_time/TogetherTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tt.Together_time;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TogetherTimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
