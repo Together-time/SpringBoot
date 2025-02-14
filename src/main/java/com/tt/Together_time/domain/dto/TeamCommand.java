1b4248ab281c24478cff82e5a44a7185ea816498
karam Choi <riverchoi2523@gmail.com>
2025-02-14 20:26:43 +0900

    [ADD] 직렬화 문제 해결

--- src/main/java/com/tt/Together_time/domain/dto/TeamCommand.java
+++ src/main/java/com/tt/Together_time/domain/dto/TeamCommand.java
@@ -3,8 +3,6 @@ package com.tt.Together_time.domain.dto;
 import com.tt.Together_time.domain.rdb.Member;
 import lombok.Getter;
 
-import java.util.List;
-
 @Getter
 public class TeamCommand {
     private Member member;
