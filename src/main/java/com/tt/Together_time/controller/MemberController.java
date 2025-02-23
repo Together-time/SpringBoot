163ea3c71c2959f841e5532ba46f4570b76c8ac0
karam Choi <riverchoi2523@gmail.com>
2025-02-23 15:24:38 +0900

    [FIX] 로그인한 사용자 이메일, 닉네임 정보 반환

--- src/main/java/com/tt/Together_time/controller/MemberController.java
+++ src/main/java/com/tt/Together_time/controller/MemberController.java
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -24,9 +26,13 @@ public class MemberController {
     }
 
     @GetMapping("/user")
-    public ResponseEntity<String> getUserInfo() {
+    public ResponseEntity<Map<String, String>> getUserInfo() {
         String email = memberService.getUserEmail();
         Member member = memberService.findByEmail(email);
-        return ResponseEntity.ok(member.getNickname());
+
+        Map<String, String> userInfoMap = new HashMap<>();
+        userInfoMap.put("email", email);
+        userInfoMap.put("nickname", member.getNickname());
+        return ResponseEntity.ok(userInfoMap);
     }
 }
