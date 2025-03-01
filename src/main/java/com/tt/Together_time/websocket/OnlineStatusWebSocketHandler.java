fbaeb4f9af8575434cb0f726dfe7e4a6d489909a
karam Choi <riverchoi2523@gmail.com>
2025-03-01 17:03:51 +0900

    [FIX] 사용자 온라인 상태 체크

--- src/main/java/com/tt/Together_time/websocket/OnlineStatusWebSocketHandler.java
+++ src/main/java/com/tt/Together_time/websocket/OnlineStatusWebSocketHandler.java
@@ -1,9 +1,8 @@
 package com.tt.Together_time.websocket;
 
-import com.tt.Together_time.service.MemberService;
 import com.tt.Together_time.service.OnlineStatusService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.Authentication;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Component;
@@ -12,24 +11,22 @@ import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class OnlineStatusWebSocketHandler extends TextWebSocketHandler {
     private final OnlineStatusService onlineStatusService;
-    private final MemberService memberService;
     private final Set<WebSocketSession> sessions = ConcurrentHashMap.newKeySet();
 
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-        String email = "";
-        Authentication authentication = (Authentication) session.getPrincipal();
-        if (authentication instanceof OAuth2AuthenticationToken) {
-            OAuth2User oauth2User = (OAuth2User) ((OAuth2AuthenticationToken) authentication).getPrincipal();
-            email = oauth2User.getAttribute("email");  // 카카오 로그인 시 이메일 가져오기
-        }
-
+        OAuth2AuthenticationToken authenticationToken = (OAuth2AuthenticationToken) session.getPrincipal();
+        OAuth2User oAuth2User = authenticationToken.getPrincipal();
+        Map<String, Object> kakaoAccount = oAuth2User.getAttribute("kakao_account");
+        String email = (String) kakaoAccount.get("email");
         onlineStatusService.setOnline(email);
         sessions.add(session);
         broadcastOnlineStatus(email, true);
@@ -37,7 +34,7 @@ public class OnlineStatusWebSocketHandler extends TextWebSocketHandler {
     //연결 종료
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
         String email = session.getPrincipal().getName();
-        onlineStatusService.setOffline(email);  // 온라인 상태 제거
+        onlineStatusService.setOffline(email);
         sessions.remove(session);
         broadcastOnlineStatus(email, false);
     }
